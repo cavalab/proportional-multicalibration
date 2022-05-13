6b2e1344c9a7f4ec3f980b5a13a23869982091e5
lacava <williamlacava@gmail.com>
2022-05-13 15:18:15 -0400

    update param space

--- experiment/ml/pmc/params.py
+++ experiment/ml/pmc/params.py
@@ -13,21 +13,28 @@ params = dict(
     max_iters=10**5,
     verbosity=1,
     n_bins=5,
+    split=0,
     # iter_sample='bootstrap'
 )
-# Etas = [0.1, 0.5, 1.0]
-Etas = scipy.stats.uniform(0.01,1)
-# Gammas = [0.1, 0.5, 1.0]
-Gammas = scipy.stats.uniform(0.05,.2)
-# Alphas = [0.01, 0.05, 0.1]
-Alphas = scipy.stats.uniform(0.01,.15)
-# N_binses = [2, 5, 10]
+Etas = [0.1, 0.5, 1.0]
+eta_dist = scipy.stats.uniform(0.01,1)
+
+Gammas = [0.1, 0.5, 1.0]
+gamma_dist = scipy.stats.uniform(0.05,.2)
+
+Alphas = [0.01, 0.05, 0.1]
+alpha_dist = scipy.stats.uniform(0.01,.15)
+
+N_binses = [2, 5, 10]
 N_binses = scipy.stats.randint(2,10)
-# Rhos = [0.05, 0.1, 0.2]
-Rhos = scipy.stats.uniform(0.05,.15)
+
+Rhos = [0.01, 0.05, 0.1, 0.2]
+rho_dist = scipy.stats.uniform(0.05,.15)
+
 Iter_samples = [None, 'bootstrap']
-# Splits = [.25, 0.5, 0.75]
-Splits = scipy.stats.uniform(0.25,.75)
+
+Splits = [.25, 0.5, 0.75]
+split_dist = scipy.stats.uniform(0.25,.75)
 
 mc_hyper_params = dict(
     eta=Etas,
