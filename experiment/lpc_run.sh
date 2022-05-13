9c251a0ae384793c7d80c113012e46276fb3e882
lacava <williamlacava@gmail.com>
2022-05-13 12:05:59 -0400

    lpc run updates

---
+++ experiment/lpc_run.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/bash
+# Run experiment. 
+rdir="results_22-05-13"
+
+alpha=0.01
+gamma=0.01
+rho=0.01
+n_bins=10
+
+methods=(
+    "lr"
+    "lr_cv"
+    "lr_mc"
+    "lr_pmc"
+    "lr_mc_cv"
+    "lr_pmc_cv"
+    "xgb"
+    "xgb_cv"
+    "xgb_mc"
+    "xgb_pmc"
+    "xgb_mc_cv"
+    "xgb_pmc_cv"
+)
+seeds=$(cat seeds.txt)
+# Job parameters
+# cores
+N=4
+q="epistasis_long"
+# mem=16384
+# mem=8192
+mem=4096
+
+timeout=3600
+
+for s in ${seeds[@]} ; do
+    for m in ${methods[@]} ; do
+        ((i=i%N)); ((i++==0)) && wait
+        echo "python evaluate_model.py data/mimic4_admissions.csv -ml $m -seed $s -alpha $alpha -gamma $gamma -n_bins $n_bins -rho $rho -results_path $rdir"
+        job_name="${m}_seed${s}_alpha${alpha}_gamma${gamma}_n_bins${n_bins}_rho${rho}" 
+        job_file="${rdir}/${job_name}" 
+
+        echo bsub -o "${job_file}.out" \
+             -n $N \
+             -J $job_name \
+             -q $q \
+             -R "span[hosts=1] rusage[mem=${mem}]" \
+             -W $timeout \
+             -M $mem \
+             python evaluate_model.py data/mimic4_admissions.csv \
+                 -ml $m \
+                 -seed $s \
+                 -alpha $alpha \
+                 -gamma $gamma \
+                 -n_bins $n_bins \
+                 -rho $rho \
+                 -results_path $rdir
+    done
+done
+
