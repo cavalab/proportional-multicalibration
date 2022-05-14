18ed7cc31cf2b6c243eb263110f832ddad508d74
lacava <williamlacava@gmail.com>
2022-05-14 14:32:33 -0400

    add rf

---
+++ experiment/ml/rf.py
@@ -0,0 +1,16 @@
+import xgboost
+
+hyper_params = [
+    {
+        'n_estimators' : (100,500),
+        # 'learning_rate' : (0.0001,0.01, 0.1 ),
+        # 'gamma' : (0.1,0.4),
+        # 'subsample' : (0.5, 1),
+    },
+]
+
+est=xgboost.XGBRFClassifier(
+    max_depth=6, 
+    eval_metric='logloss',
+    use_label_encoder=False
+)
