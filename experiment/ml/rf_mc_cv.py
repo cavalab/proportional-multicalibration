85afb687f708766f3d12b3288464bc4deebdf50e
lacava <williamlacava@gmail.com>
2022-05-17 11:13:23 -0400

    add rf cv versions

---
+++ experiment/ml/rf_mc_cv.py
@@ -0,0 +1,21 @@
+from .rf_mc import est as base_est
+from .rf_mc import hyper_params
+# from .cv.randhalving import CV,params
+from .cv.halving import CV,params
+from .pmc.metrics import multicalibration_score
+from sklearn.metrics import make_scorer
+
+base_est.verbosity=0
+
+# est = CV(base_est,
+#          param_distributions=hyper_params,
+#          **params,
+#          resource='n_samples',
+#          min_resources=10000,
+#          # max_resources=10**5,
+#         )
+est = CV(
+        base_est,
+        param_grid = hyper_params, 
+        **params
+        )
